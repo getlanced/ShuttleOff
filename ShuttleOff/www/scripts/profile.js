48f64a9fe3cf2d66d26ac4eb2995d592aad952b1
getlanced <henrymc381@gmail.com>
2020-09-23 14:50:17 +0800

    Add files via upload

--- ShuttleOff/www/scripts/profile.js
+++ ShuttleOff/www/scripts/profile.js
@@ -7,3 +7,15 @@ function closeNav(){
     var x = document.getElementById("mySidenav");
     x.style.width = "0";
 }
+
+function closeEditProfile(){
+    var x = document.getElementById("edit-profile");
+    x.style.width = "0";
+
+}
+
+function openEditProfile(){
+    var x = document.getElementById("edit-profile");
+    x.style.width = "100%";
+
+}
